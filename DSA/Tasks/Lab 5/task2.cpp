62f1d18889e733ed25ced59f78231e1440314e73
Hanzla Ahmad <hanzlaahmad100@gmail.com>
2025-04-17 12:24:38 +0500

    Done

---
+++ DSA/Tasks/Lab 5/task2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSortArr (int arr[], int n) {
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j]< arr[j+ 1])
+            {
+                swap(arr[j], arr[j+1]);
+            }
+            
+        }
+        
+    }
+    
+}
+
+void insertionSortArr (int arr[], int n) {
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] < key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    
+}
+
+void printArr (int arr[], int n) {
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+}
+
+int main() {
+    int size = 6;
+    int arrBubble[size] = {5, 2, 9, 1, 5, 6};
+
+    cout << "Bubble: Before sorting: ";
+    printArr(arrBubble, size);
+
+    bubbleSortArr(arrBubble, size);
+    
+    cout << "Bubble: After sorting: ";
+    printArr(arrBubble, size);
+
+    int arrInsertion[size] = {5, 2, 9, 1, 5, 6};
+
+    cout << "Insertion: Before sorting: ";
+    printArr(arrInsertion, size);
+
+    insertionSortArr(arrInsertion, size);
+    
+    cout << "Insertion: After sorting: ";
+    printArr(arrInsertion, size);
+    return 0;
+
+}
